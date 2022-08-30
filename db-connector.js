847cb9bb72eab6abf2990f832738eb4dc38bf432
Nitish Pal <76246219+NitishPal2013@users.noreply.github.com>
2022-08-30 21:13:13 +0530

    Update db-connector.js

--- db-connector.js
+++ db-connector.js
@@ -4,6 +4,6 @@ import fastifyMongodb from '@fastify/mongodb';
 
 export default fastifyPlugin( async function dbConnector (fastify, options) {
     fastify.register(fastifyMongodb,{
-        url: 'mongodb+srv://NitishPal:Nitish2013@cluster0.tbk31rd.mongodb.net/?retryWrites=true&w=majority'
+        url: 'The uri provided by the mongodb'
     })
 })
